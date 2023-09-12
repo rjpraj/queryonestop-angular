1b15e271ee807f796116396e94d29c161493e349
Rajesh Prajapati <pa.rajesh98@gmail.com>
2023-09-13 00:22:12 +0530

    save token in http local storage to remain log in

--- src/app/auth-services/auth-service/auth.service.ts
+++ src/app/auth-services/auth-service/auth.service.ts
@@ -1,19 +1,45 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
+import { StorageService } from '../storage-service/storage.service';
 
 const BASIC_URL = ['http://localhost:8080/']
+export const AUTH_HEADER = "authorization";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private storage:StorageService) { }
   signup(signupRequest:any):Observable<any>{
     return this.http.post(BASIC_URL+"signup",signupRequest);
   }
-  login(signupRequest:any):Observable<any>{
-    return this.http.post(BASIC_URL+"authentication",signupRequest);
+  login(loginRequest:any):Observable<any>{
+    return this.http.post(BASIC_URL+"authentication",loginRequest,
+    {observe : 'response'}) // used to same the token in http local strorage 
+    .pipe(
+      tap((__: any) => this.log("User Authentication")),
+      map((res: HttpResponse<any>) =>{
+        this.storage.saveUser(res.body);
+        const tokenLengthtemp = res.headers.get(AUTH_HEADER);
+        if(tokenLengthtemp!=null){
+          const tokenLength = tokenLengthtemp.length;
+          const bearerToken = tokenLengthtemp.substring(7,tokenLength);
+          this.storage.saveToken(bearerToken);
+          return res;
+        }
+        else{
+          console.log("token is null, please generate a valid jwt token");
+          return null;
+        }
+        
+      })
+    );
+  }
+
+  log(message:string): void{
+    console.log("User Auth Servive "+ message);
   }
 }
